ccb4f42d6069efb667e82b57ccaa95fea4cad168
Leticia Machado <leticia.machado50@gmail.com>
2019-10-05 09:33:27 -0300

    Criacao de projeto

---
+++ xadrez (console)/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrez__console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
